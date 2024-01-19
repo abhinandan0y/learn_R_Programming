fc17c7ff0ae7f88632ce121015d09b2f876fb1f7
AbhinandanYadav <138775457+abhinandan0y@users.noreply.github.com>
2024-01-19 16:59:11 +0530

    Update and rename ImportingData.R to ImportingData.m

--- ImportingData.m
+++ ImportingData.m
@@ -2,29 +2,34 @@ Importing data into R:
 
 Reading a FASTQ file from Next-Generation Sequencing (NGS) data.
 
-```R
+```
+R
 
 library(Bioconductor)
 fastq_data <- readFastq("path/to/your/file.fastq")
 Loading a CSV file containing gene expression data.
 ```
-```R
+```
+R
 
 gene_expression <- read.csv("path/to/your/gene_expression_data.csv")
 Importing a genomic annotation file in GTF format.
 ```
-```R
+```
+R
 
 library(rtracklayer)
 annotation_data <- import("path/to/your/annotation.gtf")
 Reading a BED file with genomic coordinates.
 ```
-```R
+```
+R
 
 bed_data <- read.table("path/to/your/file.bed", header = TRUE)
 Importing a VCF (Variant Call Format) file for genetic variation data.
 ```
-```R
+```
+R
 
 library(VariantAnnotation)
 vcf_data <- readVcf("path/to/your/file.vcf")
